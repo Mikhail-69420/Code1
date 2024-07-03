09bb59b88115fbf66ea6aeaa9099348b69e0ff6b
Mikhail-69420 <leederko@gmail.com>
2024-07-03 10:55:58 +0200

    change temp

---
+++ task_6/canvas_intro/canvas_intro.ts
@@ -0,0 +1,4 @@
+let canvas: HTMLCanvasElement = document.querySelector("canvas_intro");
+let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+crc2.fillStyle = "#FF0000";
+crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
